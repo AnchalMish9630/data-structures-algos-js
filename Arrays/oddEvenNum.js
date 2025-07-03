6b3d4335bf1eb31b2c349165dabc6f216ad3aa3d
AnchalMish9630 <anchalmish2025@gmail.com>
2025-07-03 23:55:46 +0530

    odd even

---
+++ Arrays/oddEvenNum.js
@@ -0,0 +1,24 @@
+// ............................  Odd number       ....................................
+let arr = [10, 5, 7, 8, 0, 8, 3, 9, 13, 21];
+
+// Loop through each element
+for (let i = 0; i < arr.length; i++) {
+    // Check if the number is odd (not divisible by 2)
+    if (arr[i] % 2 !== 0) {
+        console.log(arr[i]); // Print odd number
+    }
+}
+
+
+
+// ............................  Even number       ....................................
+let arr1 = [10, 5, 7, 8, 0, 8, 3, 9, 13, 21];
+
+// Loop through each element
+for (let i = 0; i < arr1.length; i++) {
+    // Check if the number is even ( divisible by 2)
+    if (arr1[i] % 2 !== 0) {
+        console.log(arr1[i]); // Print even
+    }
+}
+
