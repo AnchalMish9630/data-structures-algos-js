44c0ca7a8bca0f036b76866616b48a74bfa6975b
AnchalMish9630 <anchalmish2025@gmail.com>
2025-07-10 23:54:29 +0530

    revisit

--- JS Basics/functions.js
+++ JS Basics/functions.js
@@ -14,3 +14,49 @@ console.log(greetName);
 // JavaScript will overwrite the first one with the second one, 
 // because functions declared with the same name will not coexist — the last one wins.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const github_api= "https://github.com/AnchalMish9630"
+const user = fetch(github_api);
+console.log(user);
+
+
+const cart=["shirt", "dhoti","kurta"]
+const pr = createOrder (cart);
+pr.then(function(){
+   console.log("print", cart)
+})
+
+function createOrder (cart){
+    const pr = new Promise((resolve, reject)=>{
+        if(!validCart(cart)){
+            const err=new Error("Not valid")
+            reject(err)
+        }
+        const orderId = "1233"
+        if(orderId){
+            resolve(orderId)
+        }
+    })
+    return pr;
+
+}
+
+function validCart(cart){
+    return true
+}
+
