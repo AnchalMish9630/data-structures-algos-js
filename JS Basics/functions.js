6dd643a28b804f8e6d4ea77ebd0503bf63b933f2
AnchalMish9630 <anchalmish2025@gmail.com>
2025-07-12 23:52:46 +0530

    JS basic revision

--- JS Basics/functions.js
+++ JS Basics/functions.js
@@ -1,20 +1,18 @@
-function greetName1(name){
-    console.log("Namaste" + " " +name)    // function is doing console 
-}
-greetName1("Anchal")
+// function greetName1(name){
+//     console.log("Namaste" + " " +name)    // function is doing console 
+// }
+// greetName1("Anchal")
 
-// returning the value from the funtion 
+// // returning the value from the funtion 
 
-function greet(name){
-    return("Namaste" + " " +name)  // function is returning the value 
-}
-const greetName = greet("KZ");
-console.log(greetName);
+// function greet(name){
+//     return("Namaste" + " " +name)  // function is returning the value 
+// }
+// const greetName = greet("KZ");
+// console.log(greetName);
 
-// JavaScript will overwrite the first one with the second one, 
-// because functions declared with the same name will not coexist — the last one wins.
-
-
+// // JavaScript will overwrite the first one with the second one, 
+// // because functions declared with the same name will not coexist — the last one wins.
 
 
 
@@ -30,39 +28,83 @@ console.log(greetName);
 
 
 
-const github_api= "https://github.com/AnchalMish9630"
-const user = fetch(github_api);
-console.log(user);
 
 
-const cart=["shirt", "dhoti","kurta"]
-const pr = createOrder (cart);
-pr.then(function(orderId){
-   console.log("print", orderId)
-})
-.catch(function(err){
-    console.log(err)
-})
+// const github_api= "https://github.com/AnchalMish9630"
+// const user = fetch(github_api);
+// console.log(user);
 
-function createOrder (cart){
-    const pr = new Promise((resolve, reject)=>{
-        if(!validCart(cart)){
-            const err=new Error("Not valid")
-            reject(err)
-        }
-        const orderId = "1233"
-        if(orderId){
-            setTimeout(()=>{
- resolve(orderId)
-            },8000)
+
+// const cart=["shirt", "dhoti","kurta"]
+// const pr = createOrder (cart);
+// pr.then(function(orderId){
+//    console.log("print", orderId)
+// })
+// .catch(function(err){
+//     console.log(err)
+// })
+
+// function createOrder (cart){
+//     const pr = new Promise((resolve, reject)=>{
+//         if(!validCart(cart)){
+//             const err=new Error("Not valid")
+//             reject(err)
+//         }
+//         const orderId = "1233"
+//         if(orderId){
+//             setTimeout(()=>{
+//  resolve(orderId)
+//             },8000)
            
-        }
-    })
-    return pr;
+//         }
+//     })
+//     return pr;
 
-}
+// }
 
-function validCart(cart){
-    return false
-}
+// function validCart(cart){
+//     return false
+// }
+
+
+// alert(3+3)
+// console.log(typeof null)
+// console.log(typeof undefined);
+
+
+
+
+
+// let score = undefined
+// console.log(typeof score);
+
+// let  valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof score);
+
+
+// console.log("1"+2); // 12
+// console.log("1"+"2"); // 12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); // 32
+// console.log(1+2+"2"+"2");// 322
+// console.log(1+2+3+"2"+"2"); //622
+
+
+
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null>=0);
+
+console.log(undefined  == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// console.log(null>0);
+// console.log(null>0);
+
+
+
 
