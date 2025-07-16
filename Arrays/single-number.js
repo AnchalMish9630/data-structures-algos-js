82712203c7138e3fbea431f9f0f73fcc9cf843c5
AnchalMish9630 <anchalmish2025@gmail.com>
2025-07-16 23:46:16 +0530

    Single number in array

---
+++ Arrays/single-number.js
@@ -0,0 +1,23 @@
+
+// using hasmap object
+var singleNumber = function(nums) {
+   let hash={}
+   for(let i=0;i<nums.length;i++){
+    if(!hash[nums[i]]){
+        hash[nums[i]]=1
+    }
+    else{
+         hash[nums[i]]++
+    }
+     for(let i=0;i<nums.length;i++){
+        if( hash[nums[i]]==1){
+            return nums[i];
+        }
+     }
+   }
+};
+
+
+// using XOR operation
+
+
